0b529ca047c6480ef2458a863bbb45d85ff1dc18
Andrew Wong <andrew12345@live.com>
2016-12-05 21:46:09 +1100

    Consolidate source

--- Assessments/AS1 - RAD Database + Website Project (15pct)/JS/site.js
+++ Assessments/AS1 - RAD Database + Website Project (15pct)/JS/site.js
@@ -1,29 +1,38 @@
-/* Parse page arguments */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Filename: site.js                                   *
+ * Andrew Wong 2016                                    *
+ * https://github.com/bearbear12345/school_2017_12SDD  *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+/* Parse url search arguments */
 var queryString = "";
 if (window.location.search.length > 1) queryString = window.location.search.substring(1);
-var queryArray = queryString.split ("&");
+var queryArray = queryString.split("&");
 
- queryDict = {};
-for(var i in queryArray) { 
-    var key = queryArray[i].split("=");
-    if (key.length > 1) {
-      queryDict[decodeURIComponent(key[0].replace(/\+/g, " "))] = decodeURIComponent(key[1].replace(/\+/g, " "));
-    }
+queryDict = {}; // Expose the key dict pair to the page
+for (var i in queryArray) {
+	var key = queryArray[i].split("=");
+	if (key.length > 1) {
+		queryDict[decodeURIComponent(key[0].replace(/\+/g, " "))] = decodeURIComponent(key[1].replace(/\+/g, " "));
+	}
 }
+
 function buildQuery() {
-var result = ""
-for(var key in queryDict){
-result += "&" +  key + "=" + queryDict[key];
-}
-return window.location.search = result.substr(1);
+	var result = ""
+	for (var key in queryDict) {
+		result += "&" + key + "=" + queryDict[key];
+	}
+	return window.location.search = result.substr(1);
 }
 
-document.getElementById('headername').onclick=buildFunction("nasc","ndsc");
-document.getElementById('headerpublisher').onclick=buildFunction("pasc","pdsc");
-document.getElementById('headercategory').onclick=buildFunction("casc","cdsc");
+/* Handler for sorting links */
+document.getElementById('headername').onclick = buildFunction("nasc", "ndsc");
+document.getElementById('headerpublisher').onclick = buildFunction("pasc", "pdsc");
+document.getElementById('headercategory').onclick = buildFunction("casc", "cdsc");
 
 function buildFunction(strA, strB) {
-return function(){
-queryDict['sort'] = (queryDict.hasOwnProperty('sort') && queryDict['sort'].toLowerCase() == strA) ? strB : strA; buildQuery();
-}
+	return function () {
+		queryDict['sort'] = (queryDict.hasOwnProperty('sort') && queryDict['sort'].toLowerCase() == strA) ? strB : strA;
+		buildQuery();
+	}
 }
--- Assessments/AS1 - RAD Database + Website Project (15pct)/LIB/dbconnect.php
+++ Assessments/AS1 - RAD Database + Website Project (15pct)/LIB/dbconnect.php
@@ -1,8 +1,13 @@
 <?php
-$db_server = "localhost";
-$db_database = "andreww";
-$db_username = "andreww";
-$db_password = "andrew";
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Filename: dbconnect.php                             *
+ * Andrew Wong 2016                                    *
+ * https://github.com/bearbear12345/school_2017_12SDD  *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+$db_server = "localhost"; // Connect to server at localhost:3306
+$db_username = "andreww"; // Login as user "andreww"
+$db_password = "andrew";  // Authenticate with password "andrew"
+$db_database = "andreww"; // Open database "andreww"
 
 $db = mysql_connect($db_server, $db_username, $db_password);
 mysql_select_db($db_database)
