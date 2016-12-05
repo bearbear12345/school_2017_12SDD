768d8e70c6056df1b8912b4b0cff065d85655a61
Andrew Wong <andrew12345@live.com>
2016-12-06 02:30:51 +1100

    Code refactoring, consolidation, php -> html file output looks nice, add fuzzy search

--- Assessments/AS1 - RAD Database + Website Project (15pct)/JS/site.js
+++ Assessments/AS1 - RAD Database + Website Project (15pct)/JS/site.js
@@ -11,28 +11,42 @@ var queryArray = queryString.split("&");
 
 queryDict = {}; // Expose the key dict pair to the page
 for (var i in queryArray) {
-	var key = queryArray[i].split("=");
-	if (key.length > 1) {
-		queryDict[decodeURIComponent(key[0].replace(/\+/g, " "))] = decodeURIComponent(key[1].replace(/\+/g, " "));
-	}
+    var key = queryArray[i].split("=");
+    if (key.length > 1) {
+        queryDict[decodeURIComponent(key[0].replace(/\+/g, " "))] = decodeURIComponent(key[1].replace(/\+/g, " "));
+    }
 }
 
 function buildQuery() {
-	var result = ""
-	for (var key in queryDict) {
-		result += "&" + key + "=" + queryDict[key];
-	}
-	return window.location.search = result.substr(1);
+    var result = ""
+    for (var key in queryDict) {
+        result += "&" + key + "=" + queryDict[key];
+    }
+    return window.location.search = result.substr(1);
 }
 
 /* Handler for sorting links */
 document.getElementById('headername').onclick = buildFunction("nasc", "ndsc");
 document.getElementById('headerpublisher').onclick = buildFunction("pasc", "pdsc");
-document.getElementById('headercategory').onclick = buildFunction("casc", "cdsc");
+if (d = document.getElementById('headercategory')) d.onclick = buildFunction("casc", "cdsc");
 
 function buildFunction(strA, strB) {
-	return function () {
-		queryDict['sort'] = (queryDict.hasOwnProperty('sort') && queryDict['sort'].toLowerCase() == strA) ? strB : strA;
-		buildQuery();
-	}
+    return function () {
+        queryDict['sort'] = (queryDict.hasOwnProperty('sort') && queryDict['sort'].toLowerCase() == strA) ? strB : strA;
+        buildQuery();
+    }
+}
+
+var filterSearch = new List('content', {
+    valueNames: ['searchName', 'searchPublisher', 'searchCategory'],
+    plugins: [ListFuzzySearch()]
+});
+
+var elemSearchFilter = document.getElementById("searchFilter");
+elemSearchFilter.value = window.location.hash.substr(1); // Update search filter text ON PAGE LOAD
+elemSearchFilter.oninput = function () {window.location.hash = this.value;} // Event listener to update window hash on search filter text change
+window.onhashchange = function () {
+    // Perform search and update search filter text
+    filterSearch.search(hash = window.location.hash.substr(1)); // Here to update when the user manually enters a search query into the URI
+    elemSearchFilter.value = hash;
 }
